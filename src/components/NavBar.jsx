3e27c9029ab12e4bb8c05cf5e48ef1c301a6b763
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-03 03:28:25 +0700

    feat: add brand to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import image from '../helpers/image';
 import useScrollHeight from '../hooks/useScrollHeight';
 
 const NavBar = () => {
@@ -6,8 +7,10 @@ const NavBar = () => {
 
   return (
     <nav class={_height > 80 ? 'navbar active' : 'navbar unactive' }>
-      <li>Hello 1</li>
-      <li>Hello 2</li>
+      <div className="flex flex-row justify-center items-center">
+        <img class="w-16 h-16 mr-4" src={image.load(_height > 80 ? 'brand' : 'brand-white')} />
+        <h1 class={_height > 80 ? 'active' : ''}>Asccent Automobile</h1>
+      </div>
     </nav>
   );
 };
