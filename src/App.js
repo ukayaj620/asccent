dfb339ba8783e1ee5296d6f39ab0e53c6097f153
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-03 04:00:52 +0700

    feat: bind home to router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import image from './helpers/image';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className="relative flex flex-col w-full">
-      <NavBar />
-      <img style={{ marginTop: '-5rem' }} src={image.load('background')}/>
+    <div className="flex flex-col justify-between min-h-screen">
+      <Helmet titleTemplate="Codenitiva | %s" defaultTitle="Codenitiva">
+        <meta name="description" content="Codenitiva, Platform for Developers to sharing knowledge" />
+      </Helmet>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
