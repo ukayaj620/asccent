0396b304fa07b48eaccec4be151e26f2f3f1b215
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-03 03:22:33 +0700

    feat: add and load brand logo

--- src/helpers/image.js
+++ src/helpers/image.js
@@ -3,6 +3,10 @@ export default {
     switch (name) {
       case 'background':
         return require('../assets/background.svg');
+      case 'brand':
+        return require('../assets/asccent.svg');
+      case 'brand-white':
+        return require('../assets/asccent-transparent.svg');
       default:
         return null;
     };
