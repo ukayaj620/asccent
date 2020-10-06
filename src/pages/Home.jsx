e736350ad2179eb4eb2ce347d49ed8696ad1e5f3
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-06 19:44:11 +0700

    feat: add navbar to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
 import image from '../helpers/image';
 
 const Home = () => {
   return (
     <div>
+      <NavBar />
       <img style={{ marginTop: '-5rem' }} src={image.load('background')}/>
     </div>
   );
