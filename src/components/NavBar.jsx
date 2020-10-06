0b84930c3f0f219fcf9e2af1f172a79bb03532f6
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-06 19:46:24 +0700

    feat: add drawer component and responsive feature

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import image from '../helpers/image';
 import useScrollHeight from '../hooks/useScrollHeight';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useHistory } from 'react-router-dom';
+
+const Drawer = ({ open, close }) => {
+  const _history = useHistory();
+  const _location = useLocation();
+
+  const _activePath = (path) => path === _location.pathname;
+  const _isLastLabel = (index) => index === headerLinks.length-1;
+
+  return (
+    <div className="absolute left-0 top-0">
+      <div 
+        className={`flex flex-col p-4 lg:hidden absolute transition duration-200 bg-gray-100 
+          left-0 top-0 transform h-screen w-64 z-20 ${open ? 'translate-x-0' : '-translate-x-full'}`} 
+      >
+        <div onClick={close} className="self-end mb-4">
+          <img
+            className="w-6"
+            src={image.load('cross')}
+            alt="Close"
+          />
+        </div>
+        {headerLinks.map(({ name, path }, index) => (
+          <NavLink 
+            key={`#nav-link-${name}-${index}`}
+            to={path}
+            className={`
+              active border-t-2 py-2 text-base
+              ${_isLastLabel(index) ? 'border-b-2' : ''}
+            `}
+            activeClassName="link"
+            isActive={() => _activePath(path)}
+          >
+            {name}
+          </NavLink>
+        ))}
+      </div>
+      <div
+        onClick={close}
+        className={`bg-muted absolute top-0 left-0 opacity-75 z-10 w-screen h-screen lg:hidden 
+        ${open ? 'block' : 'hidden'}`} 
+      />
+    </div>
+  );
+};
 
 const NavBar = () => {
+  const [_drawerOpen, _setDrawerOpen] = useState(false);
   const _height = useScrollHeight();
   const _location = useLocation();
 
   const _activePath = (path) => path === _location.pathname;
+  const _openDrawer = () => _setDrawerOpen(true);
+  const _closeDrawer = () => _setDrawerOpen(false);
 
   return (
-    <nav class={_height > 80 ? 'navbar active' : 'navbar unactive' }>
+    <nav class={`flex-row-reverse lg:flex-row ${_height > 80 ? 'navbar active' : 'navbar unactive'}`}>
       <div className="flex flex-row justify-center items-center">
-        <img className="w-16 h-16 mr-4" src={image.load(_height > 80 ? 'brand' : 'brand-white')} />
-        <h1 className={_height > 80 ? 'active' : ''}>Asccent Automobile</h1>
+        <img className="w-16 h-16 lg:mr-4" src={image.load(_height > 80 ? 'brand' : 'brand-white')} />
+        <h1 className={`hidden lg:flex ${_height > 80 ? 'active' : ''}`}>Asccent Automobile</h1>
       </div>
-      <div className="flex flex-row justify-center items-center">
+      <Drawer open={_drawerOpen} close={_closeDrawer} />
+      <div onClick={_openDrawer} className="lg:hidden">
+        <img
+          className="w-8"
+          src={image.load(_height > 80 ? 'menu-active' : 'menu-unactive')}
+          alt="Menu"
+        />
+      </div>
+      <div className="hidden lg:flex flex-row justify-center items-center">
         {headerLinks.map(({ name, path }, index) => (
           <NavLink 
             key={`#nav-link-${name}-${index}`}
@@ -35,7 +90,7 @@ const NavBar = () => {
 const headerLinks = [
   {
     name: 'HOME',
-    path: '/home',
+    path: '/',
   },
   {
     name: 'ABOUT US',
