4f64c23e0d946e5006a366b2802832f70fad7ceb
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-06 19:43:54 +0700

    feat: load icons

--- src/helpers/image.js
+++ src/helpers/image.js
@@ -7,6 +7,12 @@ export default {
         return require('../assets/asccent.svg');
       case 'brand-white':
         return require('../assets/asccent-transparent.svg');
+      case 'menu-active':
+        return require('../assets/menu-active.svg');
+      case 'menu-unactive':
+        return require('../assets/menu-unactive.svg');
+      case 'cross':
+        return require('../assets/x.svg');
       default:
         return null;
     };
