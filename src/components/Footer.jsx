af349e5cc2b02aa1087829def87125b46d0562ec
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-12 06:28:44 +0700

    feat: create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-full flex flex-col lg:flex-row lg:justify-around bg-gray-700 p-8">
+      <h3 className="flex flex-col text-lg lg:text-xl text-white">
+        Copyright © Asccent, Inc. All rights reserved
+      </h3>
+    </footer>
+  );
+};
+
+export default Footer;
