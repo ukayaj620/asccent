a505c6fc9468754e4a6fe434928e8e29a98639d7
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-12 01:46:34 +0700

    feat(about): use background

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import image from '../helpers/image';
 
 const About = () => {
   return (
     <div>
-      <NavBar active />
+      <NavBar />
+      <img
+        style={{ marginTop: '-5rem' }} 
+        src={image.load('bg_about')}
+      />
     </div>
   );
 };
