95d335cb7bd6f8d3d1b358d694015d0df7192c72
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-12 03:07:29 +0700

    feat: embed youtube video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import image from '../helpers/image';
+import ReactPlayer from 'react-player';
+import variables from '../variables';
 
 const Home = () => {
   return (
@@ -11,6 +13,10 @@ const Home = () => {
         style={{ marginTop: '-5rem' }} 
         src={image.load('bg_home')}
       />
+      <div className="flex flex-col items-center video-container">
+        <h2 className="my-4">Upcoming Event</h2>
+        <ReactPlayer url={variables.asccent} />
+      </div>
     </div>
   );
 };
